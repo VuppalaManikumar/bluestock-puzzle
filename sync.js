018bb1d841a71da166d4ef056bdf199b8142230b
Manikumar VUPPALA <manikumarvv624@gmail.com>
2026-03-09 16:29:35 +0530

    Update sync.js

--- sync.js
+++ sync.js
@@ -1,1 +1,17 @@
+function syncData(){
 
+if(!navigator.onLine){
+console.log("Offline mode")
+return
+}
+
+const activity=getActivity()
+
+fetch("https://example.com/sync",{
+method:"POST",
+body:JSON.stringify(activity)
+})
+.then(()=>console.log("Sync success"))
+.catch(()=>console.log("Sync failed"))
+
+}
