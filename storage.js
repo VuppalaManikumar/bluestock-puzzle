d9baf0fdd65c1b04bb16b8c00feda8dfb985fcee
Manikumar VUPPALA <manikumarvv624@gmail.com>
2026-03-09 16:28:58 +0530

    Update storage.js

--- storage.js
+++ storage.js
@@ -1,1 +1,22 @@
+function getActivity(){
+const data=localStorage.getItem("activityData")
+if(data)return JSON.parse(data)
+return{}
+}
 
+function saveActivity(data){
+localStorage.setItem("activityData",JSON.stringify(data))
+}
+
+function saveToday(score,time){
+const activity=getActivity()
+const today=new Date().toISOString().split("T")[0]
+
+activity[today]={
+score:score,
+time:time,
+solved:true
+}
+
+saveActivity(activity)
+}
